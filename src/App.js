247dced011a69cba720cab00a4632f19471e67e4
Cody Rush <rush_cody@hotmail.com>
2020-01-28 21:37:10 -0700

    began wireframing basic logic

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WeatherContainer from './Components/WeatherContainer.jsx';
+
+function App() {
+  return (
+    <div className='app-container'>
+      <WeatherContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/WeatherContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import WeatherForm from './WeatherForm';
+
+const WeatherContainer = () => {
+  return (
+    <div>
+      <h1>Hello Weather</h1>
+      <WeatherForm />
+    </div>
+  );
+};
+
+export default WeatherContainer;
